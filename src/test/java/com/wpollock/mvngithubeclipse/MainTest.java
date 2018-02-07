673a0015d9182aa77c9d343292ed0c3c8868b7f4
Wayne Pollock <pollock@acm.org>
2018-02-07 01:36:52 -0500

    Initial commit

---
+++ src/test/java/com/wpollock/mvngithubeclipse/MainTest.java
@@ -0,0 +1,14 @@
+package com.wpollock.mvngithubeclipse;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
